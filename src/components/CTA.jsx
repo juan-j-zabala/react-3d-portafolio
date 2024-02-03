41a47f45b864985b0d1a7f8b935362234abfe4e4
juan-j-zabala <juzadev217@gmail.com>
2024-02-03 17:32:25 -0500

    developing about page ended

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <section>
-      <p>
-        Have a project in mind? <br />
+    <section className="cta">
+      <p className="cta-text">
+        Have a project in mind? <br className="sm:block hidden" />
         Let's build something together
       </p>
-      <Link to="/contact" />
+      <Link to="/contact" className="btn">
+        Contact
+      </Link>
     </section>
   );
 };
